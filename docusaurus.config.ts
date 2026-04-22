866d577a914a56af8caf057c548bd29a9c654c7e
yu <1210040102@qq.com>
2026-04-23 01:57:34 +0800

    aaaaaaaaaaaaq

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,9 +5,9 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Archlinux-guide',
+  tagline: 'ArchLinux Guide',
+  favicon: 'ico.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -31,8 +31,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
   },
 
   presets: [
@@ -89,7 +89,7 @@ const config: Config = {
           
         },
         {
-          href: 'https://github.com/SHORiN-KiWATA/Shorin-ArchLinux-Guide.wiki.git',
+          href: 'https://github.com/SHORiN-KiWATA/Shorin-ArchLinux-Guide/wiki',
           label: '原WIKI地址',
           position: 'right',
         },
